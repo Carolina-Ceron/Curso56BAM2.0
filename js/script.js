abfc1ef8187a9f639ff5840048e44c56d9cab848
Carolina-Ceron <carol.ceron801@gmail.com>
2023-11-16 19:33:36 -0300

    Cambios para entrega final

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/*let main = document.getElementsByClassName(".main-tit");
+let mainTit = document.getElementsByClassName(".tit-2");
+main.innerText(mainTit);
+let prueba = document.getElementsByTagName(div.innerText(".tit-1"))*/
+
+  /*let titulo = document.getElementsByClassName('tit-1');
+  let cambiar = document.getElementsByClassName('tit');
+  cambiar.innerText(titulo);
+  let h2 = document.querySelector('h2')
+  cambiar.innerHTML=titulo;
+  let modificable = document.getElementsByClassName('tit')[0];
+  let titulo = document.getElementsByClassName('tit-1');
+  modificable.innerHTML=titulo;*/
+
+function selectText1(){
+  document.querySelector('.tit').innerText=  "Historia";
+  document.querySelector('.main-text').innerText="El Clan Gojo es de uno de los tres grandes clanes existentes en la serie Jujutsu Kaisen. Sus miembros son desciendes del hechicero con un espíritu vengativo Sugawara-no-Michizan. Se tiene conocimiento de que la técnica transmitida entre los miembros de la familia es la Técnica de Maldición Ilimitada, sin embargo, sólo aquellos que también hayan heredado el poder de los Seis Ojos podrán hacer uso correcto de la misma. Luego de más de 400 años, con el nacimiento de Satoru Gojo, el 7 de diciembre de 1989, el clan pudo volver a ser poseedor de dichos ojos. De él ha heredado la Técnica de Maldición ilimitada y el poder de los seis ojos."; 
+}
+function selectText2(){
+  document.querySelector('.tit').innerText=  "Personalidad";
+  document.querySelector('.main-text').innerText="Satoru es una persona compleja, ya que generalmente es indiferente y juguetón con personas como sus estudiantes y compañeros de trabajo, pero antipático y cruel con los ancianos de los altos mandos del jujutsu. Tiene mucha confianza en sus habilidades y en su reputación como un poderoso hechicero.  En una crisis, Satoru puede mantenerse tranquilo. Dará prioridad a destruir a sus enemigos antes que salvar a los inocentes cuando crea que el sacrificio es inevitable. Sin embargo, esto solo se extiende a las personas asesinadas por su oponente, es decir, Satoru no dañará ni matará a ninguna persona inocente para tomar ventaja.";
+}
+function selectText3(){
+  document.querySelector('.tit').innerText=  "Aspecto";
+  document.querySelector('.main-text').innerText="Es un hombre de contextura fisica delgada y una altura de 190cm. Su cabello es de color blanco y suele llevarlo peinado hacía arriba con mechones puntiagudos, aunque en algunas ocasiones también podemos verlo con el pelo alborotado por delante. Tiene los ojos de color azul claro y suele cubrirlos con una venda negra o, cuando luce un look más informal, usa gafas de sol. En cuanto al resto de su vestimenta, la mayor parte del tiempo lleva puesto su uniforme de hechicero, el cual es totalmente negro y que consiste en una chaqueta con cremallera y cuello alto y ancho, pantalones largos a juego y unos zapatos.";
+}
+function selectText4(){
+  document.querySelector('.tit').innerText= "Habilidades";
+  document.querySelector('.main-text').innerText="Entre sus tecnicas se encuentran la Rotacion Inversa Resplandor Rojo, Rotacion hacia adelante Azul, tecnica del vacio Purpura y expansion de dominio: Vacio inconmensurable. Satoru, además de su abrumador nivel de energía maldita, también es un maestro de artes marciales, con una destreza física extraordinaria para respaldar sus habilidades. Tiene una gran fuerza física Y además posee una gran velocidad, logrando seguir los movimientos de una maldición de Grado especial sin esfuerzo."
+}
+function zoom(element){
+  element.classList.toggle("fullsize");
+}
